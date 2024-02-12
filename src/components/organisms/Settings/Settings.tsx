cec5e39090cbd5f235310dfd66ecb1e8ab667f52
mikan-mikan <51882748+mikan-mikan@users.noreply.github.com>
2024-02-12 21:49:53 +0900

    #5 Settingsコンポーネント作成、遷移できるように設定

--- src/components/organisms/Settings/Settings.tsx
+++ src/components/organisms/Settings/Settings.tsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import { CurrentScreenContext } from '@/contexts/CurrentScreenContext';
 import { CurrentType } from '@/types';
 
-function GameTop(): JSX.Element {
+function Settings(): JSX.Element {
   const current = useContext(CurrentScreenContext);
 
   function goToPageButton(page: CurrentType): void {
@@ -12,31 +12,23 @@ function GameTop(): JSX.Element {
 
   return (
     <div>
-      <h2 className="text-center text-4xl">スタート画面</h2>
+      <h2 className="text-center text-4xl">設定画面</h2>
+
+      {/* TODO: 効果音、BGM を設定 */}
 
       <div>
         <button
           type="button"
           onClick={() => {
-            goToPageButton('セレクト');
+            // TODO: 遷移前の画面に戻る、仮でトップ画面に戻る
+            goToPageButton('トップ');
           }}
         >
-          スタートする
-        </button>
-      </div>
-
-      <div>
-        <button
-          type="button"
-          onClick={() => {
-            goToPageButton('セッティング');
-          }}
-        >
-          設定画面
+          戻る
         </button>
       </div>
     </div>
   );
 }
 
-export default GameTop;
+export default Settings;
