47001727292f3dc349e7f174b5c703ab92cab673
mikan-mikan <51882748+mikan-mikan@users.noreply.github.com>
2024-02-17 23:01:57 +0900

    #5 Typing画面のミスだった場合、その他変数名等を調整

--- src/components/organisms/TypingGame/TypingGame.tsx
+++ src/components/organisms/TypingGame/TypingGame.tsx
@@ -6,19 +6,73 @@ import * as wanakana from 'wanakana';
 import CountDown from '@/components/parts/CountDown/CountDown';
 import { CourseLevelContext } from '@/contexts/CourseLevelContext';
 import { CurrentScreenContext } from '@/contexts/CurrentScreenContext';
+import { checkJa } from '@/functions/common';
 
 function TypingGame(): JSX.Element {
   const [isStart, setIsStart] = useState(false); // カウントダウンが終了してゲームが開始できる状態かどうか
   const jpText = 'ここに練習用のテキストを設定';
-  const exampleText = 'ここにれんしゅうようのてきすとをせってい';
-  const alphabetText = wanakana.toRomaji(exampleText);
-  const [displayText, setDisplayText] = useState(alphabetText); // 表示するテキスト
+  const exampleTextKana = 'ここにれんしゅうようのてきすとをせってい';
+  const exampleTextRomaji = wanakana.toRomaji(exampleTextKana);
+  const [displayTextRomaji, setDisplayTextRomaji] = useState(exampleTextRomaji); // 表示するテキスト
   const [userInput, setUserInput] = useState(''); // ユーザーの入力
   const [prevUserInput, setPrevUserInput] = useState(''); // 直前のユーザーの入力
   const [isOk, setIsOk] = useState(true); // ユーザーの入力が正しいかどうか
 
   const current = useContext(CurrentScreenContext);
   const { courseCo, levelCo } = useContext(CourseLevelContext);
+
+  useEffect(() => {
+    const userInputToHiragana: string = wanakana.toHiragana(userInput);
+    const displayTextToHiragana: string = wanakana.toHiragana(displayTextRomaji);
+    const isInputOK = displayTextToHiragana.startsWith(userInputToHiragana);
+
+    if (isInputOK) {
+      const remainingText = removeMatchingPrefix(displayTextToHiragana, userInputToHiragana);
+      const remainingTextToRomaji = wanakana.toRomaji(remainingText);
+      const isFinished = remainingTextToRomaji.length === 0;
+
+      // TODO: 最後の問題で、残りの文字がなくなったらゲーム終了, リザルト画面に遷移
+      if (isFinished) {
+        current.setCurrentNameCo('リザルト');
+      }
+
+      setUserInput('');
+      setDisplayTextRomaji(remainingTextToRomaji);
+      setIsOk(true);
+    } else {
+      setIsOk(false);
+
+      // 日本語入力の場合は空にする
+      const isJaInput = checkJa.test(userInput);
+      if (isJaInput) {
+        setUserInput('');
+        return;
+      }
+
+      const isLongInput = userInput.length > 2;
+      const isUserInputEmpty = isLongInput && !displayTextRomaji.startsWith(userInput);
+      if (isUserInputEmpty) {
+        setUserInput('');
+        return;
+      }
+    }
+  }, [current, userInput]);
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const { value } = event.target;
+    setUserInput(value);
+    setPrevUserInput(value);
+  }
+
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>): void {
+    if (event.key === 'Escape') {
+      current.setCurrentNameCo('セレクト');
+    }
+  }
+
+  function handleCountdownFinished(): void {
+    setIsStart(true);
+  }
 
   // 入力された文字列とパターンを比較し、一致する部分を削除した文字列を返す
   function removeMatchingPrefix(input: string, pattern: string): string {
@@ -28,61 +82,6 @@ function TypingGame(): JSX.Element {
     }
     return input.substring(index);
   }
-
-  // function checkInputStrings(input: string): string {
-  //   console.log('input:', input);
-  //   const original = input;
-  //   const toHiraganaOriginal = wanakana.toHiragana(original);
-  //   const toRomajiOriginal = wanakana.toRomaji(toHiraganaOriginal);
-  //   console.log('original:', original);
-  //   console.log('toRomajiOriginal:', toRomajiOriginal);
-  //   if (original.length - toRomajiOriginal.length >= 2) {
-  //     return original.slice(0, -1);
-  //   } else {
-  //     return original;
-  //   }
-  // }
-
-  useEffect(() => {
-    //TODO: 入力途中の1文字が残る場合の判断をしたい、一度ローマ字に変換してから判断する？①
-    // const checkUserInputText: string = checkInputStrings(userInput);
-
-    const checkUserInput: string = wanakana.toHiragana(userInput);
-
-    if (exampleText.startsWith(checkUserInput)) {
-      // exampleTextの先頭の文字を削除
-      const remainingText = removeMatchingPrefix(exampleText, checkUserInput);
-      const remainingTextToRomaji = wanakana.toRomaji(remainingText);
-      // TODO: 最後の問題で、残りの文字がなくなったらゲーム終了, リザルト画面に遷移
-      if (remainingTextToRomaji.length === 0) {
-        current.setCurrentNameCo('リザルト');
-      }
-      setDisplayText(remainingTextToRomaji);
-      setIsOk(true);
-    } else {
-      // TODO: 最後に入力した1文字を削除したい、母音が合っているかどうかを判断して削除するかどうかを判断させたい②
-      // const newQuestionText = userInput.slice(0, -1);
-      // setDisplayText(newQuestionText);
-      // setUserInput(newQuestionText);
-      setIsOk(false);
-    }
-  }, [current, userInput]);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const { value } = event.target;
-    setUserInput(value);
-    setPrevUserInput(value.slice(-1));
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
-    if (event.key === 'Escape') {
-      current.setCurrentNameCo('セレクト');
-    }
-  };
-
-  const handleCountdownFinished = (): void => {
-    setIsStart(true);
-  };
 
   return (
     <>
@@ -94,7 +93,7 @@ function TypingGame(): JSX.Element {
           <p className="mt-10">{jpText}</p>
           <p className="mt-4">
             <span className={isOk ? 'text-green-400' : 'text-red-400'}>{prevUserInput}</span>
-            {displayText}
+            {displayTextRomaji}
           </p>
           <input
             className="mt-4"
